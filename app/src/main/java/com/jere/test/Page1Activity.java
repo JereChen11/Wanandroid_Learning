725876483bfb9d98c0dbde8354d2e50ddece2d23
jere <jere@motherapp.com>
2019-10-15 22:25:33 +0800

    add bottom bar

---
+++ app/src/main/java/com/jere/test/Page1Activity.java
@@ -0,0 +1,13 @@
+package com.jere.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Page1Activity extends BaseBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1);
+    }
+}
---
+++ app/src/main/java/com/jere/test/Page3Activity.java
@@ -0,0 +1,12 @@
+package com.jere.test;
+
+import android.os.Bundle;
+
+public class Page3Activity extends BaseBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page3);
+    }
+}
