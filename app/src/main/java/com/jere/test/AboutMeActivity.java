9619367c57a9fba82757c8b0d73dde423831a748
Jere <jerechen11@gmail.com>
2020-02-14 21:30:03 +0800

    change about me to webview

--- app/src/main/java/com/jere/test/AboutMeActivity.java
+++ app/src/main/java/com/jere/test/AboutMeActivity.java
@@ -1,7 +1,9 @@
 package com.jere.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 /**
  * @author jere
@@ -12,5 +14,10 @@ public class AboutMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        WebView webView = findViewById(R.id.about_me_webview);
+        webView.loadUrl("https://blog.csdn.net/jerechen");
+        Toast.makeText(AboutMeActivity.this, "click content tv", Toast.LENGTH_SHORT).show();
+
     }
 }
