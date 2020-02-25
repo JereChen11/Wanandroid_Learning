d8a8885a2335bf86f1889ab601d7da406811ae7a
Jere <jerechen11@gmail.com>
2020-02-25 21:44:46 +0800

    add personal info custom component

---
+++ app/src/main/java/com/jere/test/customcomponent/PersonalInfoItemCustomView.java
@@ -0,0 +1,47 @@
+package com.jere.test.customcomponent;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jere.test.R;
+
+/**
+ * @author jere
+ */
+public class PersonalInfoItemCustomView extends ConstraintLayout {
+
+
+    public PersonalInfoItemCustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        inflate(context, R.layout.personal_info_item_custom_view, this);
+
+
+        TextView categoryTv = findViewById(R.id.personal_info_item_category_iv);
+        TextView contentTv = findViewById(R.id.personal_info_item_content_tv);
+        ImageView avatarIv = findViewById(R.id.personal_info_item_avatar_iv);
+
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PersonalInfoItemCustomView);
+        String categoryTextString = typedArray.getString(R.styleable.PersonalInfoItemCustomView_categoryText);
+        categoryTv.setText(categoryTextString);
+        String contentTextString = typedArray.getString(R.styleable.PersonalInfoItemCustomView_contentText);
+        contentTv.setText(contentTextString);
+        boolean isContainAvatar = typedArray.getBoolean(R.styleable.PersonalInfoItemCustomView_isDisplayImage, false);
+        if (isContainAvatar) {
+            avatarIv.setVisibility(VISIBLE);
+        }
+        int imageResourceId = typedArray.getResourceId(R.styleable.PersonalInfoItemCustomView_imageResource, R.drawable.default_portrait);
+        avatarIv.setImageResource(imageResourceId);
+
+        typedArray.recycle();
+    }
+
+}
