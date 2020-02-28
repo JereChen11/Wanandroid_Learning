bd0c182518da4c5c669a076267e3b5ff87f841bf
Jere <jerechen11@gmail.com>
2020-02-28 22:57:58 +0800

    add my account ui

---
+++ app/src/main/java/com/jere/test/customcomponent/MyAccountInfoItemCustomView.java
@@ -0,0 +1,37 @@
+package com.jere.test.customcomponent;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jere.test.R;
+
+/**
+ * @author jere
+ */
+public class MyAccountInfoItemCustomView extends ConstraintLayout {
+
+    public MyAccountInfoItemCustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        inflate(context, R.layout.my_account_info_item_custom_view, this);
+
+        TextView categoryTv = findViewById(R.id.account_info_item_category_tv);
+        ImageView categoryIconIv = findViewById(R.id.account_info_item_category_icon_iv);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyAccountInfoItemCustomView);
+        String categoryTextString = typedArray.getString(R.styleable.MyAccountInfoItemCustomView_categoryItemText);
+        categoryTv.setText(categoryTextString);
+
+        int iconResourceId = typedArray.getResourceId(R.styleable.MyAccountInfoItemCustomView_iconResource, R.drawable.my_account_icon);
+        categoryIconIv.setImageResource(iconResourceId);
+
+        typedArray.recycle();
+    }
+}
